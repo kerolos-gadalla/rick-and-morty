69a2fa7d9e740d04a1306e6ce089efb1b16d9103
kerolos <kerolos.sss@gmail.com>
2023-01-10 15:45:45 +0400

    some refactory

--- src/App.tsx
+++ src/App.tsx
@@ -1,72 +1,20 @@
-import React, { useCallback, useEffect, useState } from "react";
-import logo from "./logo.svg";
+import React, { ComponentClass } from "react";
 import "./App.css";
 import { ApolloProvider } from "@apollo/client";
-import { useCharactersQuery } from "./useCharactersQuery";
 import { graphQLClient } from "./graphQLClient";
-import InfiniteScroll from "react-infinite-scroll-component";
+import { CharactersPage } from "./CharactersPage";
 
-function AppComponent() {
-  const page = 2;
-  const { characters, info, getNext } = useCharactersQuery();
-
-  const [hasMore, setHasMore] = useState(true);
-  useEffect(() => {
-    setHasMore((hasMore) => {
-      return info?.next > 0;
-    });
-  }, [setHasMore, info]);
-
-  const getMore = useCallback(() => {
-    getNext(info?.next);
-  }, [getNext, info?.next]);
-  useEffect(() => {
-    getNext(0);
-  }, [getNext]);
+const App = () => {
   // const results = data?.characters.results;
-  return characters == null ? (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  ) : (
-    <div className="App">
-      <pre>{JSON.stringify(info, null, 2)}</pre>
-      <InfiniteScroll
-        next={getMore}
-        hasMore={hasMore}
-        loader={<h4>Loading...</h4>}
-        dataLength={info.count || 0}
-      >
-        {characters.map((character) => (
-          <div key={character.id}>
-            id: {character.id}
-            <br />
-            name: {character.name}
-            <br />
-          </div>
-        ))}
-      </InfiniteScroll>
-    </div>
+  return <CharactersPage />;
+};
+
+function withAppollo<T>(Component: React.ComponentType<T>) {
+  return (props: T & React.Attributes) => (
+    <ApolloProvider client={graphQLClient}>
+      <Component {...props} />
+    </ApolloProvider>
   );
 }
 
-const App = () => (
-  <ApolloProvider client={graphQLClient}>
-    <AppComponent />
-  </ApolloProvider>
-);
-
-export default App;
+export default withAppollo(App);
---
+++ src/CharactersPage.tsx
@@ -0,0 +1,42 @@
+import React, { useCallback, useEffect, useState } from "react";
+import { useCharactersQuery } from "./useCharactersQuery";
+import InfiniteScroll from "react-infinite-scroll-component";
+
+export function CharactersPage({ }) {
+  const { characters, info, getNext } = useCharactersQuery();
+
+  const [hasMore, setHasMore] = useState(true);
+  useEffect(() => {
+    setHasMore((hasMore) => {
+      return info?.next > 0;
+    });
+  }, [setHasMore, info]);
+
+  const getMore = useCallback(() => {
+    getNext(info?.next);
+  }, [getNext, info?.next]);
+  useEffect(() => {
+    getNext(0);
+  }, [getNext]);
+
+  return (
+    <div className="App">
+      <pre>{JSON.stringify(info, null, 2)}</pre>
+      <InfiniteScroll
+        next={getMore}
+        hasMore={hasMore}
+        loader={<h4>Loading...</h4>}
+        dataLength={info.count || 0}
+      >
+        {characters.map((character) => (
+          <div key={character.id}>
+            id: {character.id}
+            <br />
+            name: {character.name}
+            <br />
+          </div>
+        ))}
+      </InfiniteScroll>
+    </div>
+  );
+}
